a191d7011f75b0d11d6045d59e67ba09c6251ce3
twk56 <thanawatklomkliew@gmail.com>
2025-03-23 21:12:23 +0700

    Update map.py

--- map.py
+++ map.py
@@ -1,11 +1,8 @@
-# map.py
 import pygame
 from config import SCREEN_WIDTH, SCREEN_HEIGHT, BACKGROUND_IMAGE_PATH
 
-# โหลดรูปพื้นหลัง
 background_image = pygame.image.load(BACKGROUND_IMAGE_PATH)
 
 def draw_background(surface):
-    # ปรับขนาดรูปพื้นหลังให้พอดีกับหน้าจอ
     background_scaled = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     surface.blit(background_scaled, (0, 0))
